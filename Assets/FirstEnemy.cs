750b8f754c041370fe5d98bc3baad962abc7dcb4
Borgeshc <hborges@digitaliris.com>
2017-01-20 21:35:13 -0600

    Merged health and enemy ai

---
+++ Assets/FirstEnemy.cs
@@ -0,0 +1,69 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class FirstEnemy : MonoBehaviour {
+	private NavMeshAgent nav;
+	[SerializeField]
+	private List<GameObject> targets; // array of targets
+    [SerializeField]
+    private float speed; // checking distance is for when a player enters the enemy's trigger. If the current target is too far away then theTarget will become the player that the enemy collided with.
+
+	private GameObject theTarget;// the actual target
+	private int _target; // the target selector 
+                         // Use this for initialization
+    GameManager gameManager;
+	void Start ()
+    {
+        gameManager = GameObject.Find("GameManager").GetComponent<GameManager>();
+        foreach(GameObject go in gameManager.players)
+        {
+            targets.Add(go);
+        }
+		nav = GetComponent<NavMeshAgent> ();
+		_target = Random.Range (0, targets.Count);
+		theTarget = targets[_target];
+		print (theTarget);
+		Chase ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//print ("Chase");
+		if (!theTarget.activeInHierarchy)
+		{
+			for(int i = 0; i < targets.Count; i++)
+			{
+				if (targets [i].activeInHierarchy)
+					theTarget = targets [i].gameObject;
+			}
+
+			if (!theTarget.activeInHierarchy) {
+				print("I have no targets");
+				//Do nothing
+			}
+			else
+			Chase ();
+		}
+	}
+	void OnTriggerEnter(Collider other){
+		if (other.tag == "Player") 
+		{
+			Attack ();
+		}
+	}
+	void OnTriggerExit(Collider other){
+		if (other.tag == "Player") {
+				Chase ();
+		}
+	}
+	void Attack(){// adding to the attack function later on in the jam
+		//print ("attack");
+		nav.speed = 0f;
+	}
+	void Chase()
+	{
+		nav.destination = theTarget.transform.position;
+		nav.speed = speed;
+	}
+}
---
+++ Assets/Health.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Health : MonoBehaviour
+{
+    public float baseHealth;
+    float health;
+
+    void Start()
+    {
+        health = baseHealth;
+    }
+    
+    public void TookDamage(int damage)
+    {
+        health = health - damage;
+        if(health <= 0)
+        {
+            WasDestroyed();
+        }
+    }
+
+    public void WasDestroyed()
+    {
+        gameObject.SetActive(false);
+    }
+}
