d0d43f366a400d37036c437178b0457ab521120d
gebharsj <gebharsj@gmatc.matc.edu>
2017-01-20 18:41:29 -0600

    factory + character

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,91 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMovement : MonoBehaviour {
+
+    public float speed = 10f;
+    public float jumpForce = 10f;
+    float hori;
+    float vert;
+    float m_speed;
+    float groundCheckDistance = 0.2f;
+    float originalGroundCheckDistance;
+
+    bool isGrounded;
+    bool settingLandBool;
+    bool horiPressed;
+    bool vertPressed;
+
+    Animator anim;
+    Rigidbody rb;
+
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+        rb = GetComponent<Rigidbody>();
+        originalGroundCheckDistance = groundCheckDistance;
+        m_speed = speed;
+    }
+
+    void FixedUpdate()
+    {
+        hori = Input.GetAxis("Horizontal");
+        vert = Input.GetAxis("Vertical");
+
+        CheckGroundedStatus();
+
+        if (isGrounded)
+        {
+            anim.SetFloat("Horizontal", hori);
+            anim.SetFloat("Vertical", vert);
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
+        {
+            anim.SetBool("isJumping", true);
+            rb.velocity = new Vector3(rb.velocity.x, jumpForce, rb.velocity.z);
+            CheckGroundedStatus();
+        }
+
+        if (hori != 0)
+            horiPressed = true;
+        else
+            horiPressed = false;
+
+        if (vert != 0)
+            vertPressed = true;
+        else
+            vertPressed = false;
+
+        if (horiPressed && vertPressed)
+            m_speed = speed / speed;
+        else
+            m_speed = speed;
+
+        if (!isGrounded)
+            m_speed = speed * 0.75f;
+        else
+            m_speed = speed;
+
+        transform.Translate(new Vector3(hori * Time.deltaTime * m_speed, 0f, vert * Time.deltaTime * m_speed));
+    }
+
+    void CheckGroundedStatus()
+    {
+        RaycastHit hitInfo;
+        Debug.DrawLine(transform.position + (Vector3.up * 0.1f), transform.position + (Vector3.up * 0.2f) + (Vector3.down * groundCheckDistance));
+        if (Physics.Raycast(transform.position + (Vector3.up * 0.1f), Vector3.down, out hitInfo))
+        {
+            if ((hitInfo.collider.tag == "Ground" || hitInfo.collider.tag == "Environment") && hitInfo.distance <= groundCheckDistance)
+            {
+                isGrounded = true;
+            }
+            else
+            {
+                isGrounded = false;
+                anim.SetBool("isJumping", false);
+                anim.SetFloat("Height", hitInfo.distance);
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/ThirdPersonCamera.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ThirdPersonCamera : MonoBehaviour {
+	private const float Y_Angle_Min = 0.0f;
+	private const float Y_Angle_Max = 50.0f;
+	[SerializeField]
+	Transform lookAt, camTransform;
+	private Camera main;
+	Vector3 dir;
+	Quaternion rotation;
+	private float distance =10.0f, currentX = 0.0f, currentY = 0.0f;
+	// Use this for initialization
+	void Start () {
+		camTransform = transform;
+		main = Camera.main;
+	}
+	
+	// Update is called once per frame
+	void Update(){
+		currentX += Input.GetAxis ("Mouse X");
+		currentY += Input.GetAxis ("Mouse Y");
+
+		currentY = Mathf.Clamp (currentY, Y_Angle_Min, Y_Angle_Max);
+	}
+	void LateUpdate () {
+		dir = new Vector3 (0, 0, -distance);
+		rotation = Quaternion.Euler (currentY, currentX, 0);
+		camTransform.position = lookAt.position + rotation * dir;
+		camTransform.LookAt (lookAt.position);
+	}
+}
