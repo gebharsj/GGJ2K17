2a3a6307be536d51768e9ac33106f3dc9b0cf3bb
Borgeshc <hborges@digitaliris.com>
2017-01-21 23:29:54 -0600

    Merged menus

---
+++ Assets/PowerUp.cs
@@ -0,0 +1,78 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PowerUp : MonoBehaviour
+{
+    public enum PowerUpType
+    {
+        None,
+        Speed,
+        Damage,
+        SuperJump,
+        Grenade,
+    }
+
+    float originalSpeed;
+    int originalDamage;
+    float originalJumpForce;
+    float originalCooldown;
+
+    public PowerUpType type = PowerUpType.None;
+    
+    void OnTriggerEnter(Collider other)
+    {
+        if(other.name == "PlayerOne")
+        {
+            ApplyPowerUp(other.gameObject);   
+        }
+        else if (other.name == "PlayerTwo")
+        {
+            ApplyPowerUp(other.gameObject);
+        }
+    }
+
+    void ApplyPowerUp(GameObject player)
+    {
+        GetComponent<SphereCollider>().enabled = false;
+        GetComponent<MeshRenderer>().enabled = false;
+
+        switch (type)
+        {
+            case PowerUpType.None:
+                Debug.LogError("The power up type has not been assigned!");
+                break;
+            case PowerUpType.Speed:
+                originalSpeed = player.GetComponent<PlayerMovement>().speed;
+                player.GetComponent<PlayerMovement>().speed = 100;
+                StartCoroutine(PowerUpTimer());
+                player.GetComponent<PlayerMovement>().speed = originalSpeed;
+                Destroy(gameObject);
+                break;
+            //case PowerUpType.Damage:
+            //    originalDamage = player.GetComponentInChildren<Damage>().;
+            //    player.GetComponentInChildren<Damage>().damageDone *= 10;
+            //    StartCoroutine(PowerUpTimer());
+            //    player.GetComponentInChildren<Damage>().damageDone = originalDamage;
+            //    Destroy(gameObject);
+            //    break;
+            case PowerUpType.SuperJump:
+                originalJumpForce = player.GetComponent<PlayerMovement>().jumpForce;
+                player.GetComponent<PlayerMovement>().jumpForce *= 3;
+                StartCoroutine(PowerUpTimer());
+                player.GetComponent<PlayerMovement>().jumpForce = originalJumpForce;
+                Destroy(gameObject);
+                break;
+            //case PowerUpType.Grenade:
+            //    originalCooldown = player.GetComponent<TossGrenade>().cooldown;
+            //    player.GetComponent<TossGrenade>().cooldown = 1;
+            //    StartCoroutine(PowerUpTimer());
+            //    player.GetComponent<TossGrenade>().cooldown = originalCooldown;
+            //    Destroy(gameObject);
+            //    break;
+        }
+    }
+    IEnumerator PowerUpTimer()
+    {
+        yield return new WaitForSeconds(10);
+    }
+}
---
+++ Assets/Scripts/MenuScript.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class MenuScript : MonoBehaviour {
+
+	public void LoadLevel(string levelName)
+    {
+        SceneManager.LoadScene(levelName);
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+}
